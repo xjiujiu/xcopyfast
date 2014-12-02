aec31b37b82bbfda8744f76c5e0a33c689dcfd5e
xjiujiu <xjiujiu@gmail.com>
2014-12-02 21:09:25 +0800

    更新项目如下
    1. 加入项目第一版本源代码。

---
+++ XCopyFast/XCopyFast.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace XCopyFast
+{
+    partial class XCopyFast
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(XCopyFast));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SelectSrcRootDir = new System.Windows.Forms.Button();
+            this.SelectDescDir = new System.Windows.Forms.Button();
+            this.SrcRootDir = new System.Windows.Forms.TextBox();
+            this.DescDir = new System.Windows.Forms.TextBox();
+            this.SrcSubDir = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.LogTextBox = new System.Windows.Forms.TextBox();
+            this.StartCopyBtn = new System.Windows.Forms.Button();
+            this.FBDBaseDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.FBDDescDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.选项ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.帮助ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.关于本软件ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.联系作者ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.TSMIClose = new System.Windows.Forms.ToolStripMenuItem();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.Controls.Add(this.SelectSrcRootDir);
+            this.groupBox1.Controls.Add(this.SelectDescDir);
+            this.groupBox1.Controls.Add(this.SrcRootDir);
+            this.groupBox1.Controls.Add(this.DescDir);
+            this.groupBox1.Controls.Add(this.SrcSubDir);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // SelectSrcRootDir
+            // 
+            resources.ApplyResources(this.SelectSrcRootDir, "SelectSrcRootDir");
+            this.SelectSrcRootDir.Name = "SelectSrcRootDir";
+            this.SelectSrcRootDir.UseVisualStyleBackColor = true;
+            this.SelectSrcRootDir.Click += new System.EventHandler(this.SelectSrcRootDir_Click);
+            // 
+            // SelectDescDir
+            // 
+            resources.ApplyResources(this.SelectDescDir, "SelectDescDir");
+            this.SelectDescDir.Name = "SelectDescDir";
+            this.SelectDescDir.UseVisualStyleBackColor = true;
+            this.SelectDescDir.Click += new System.EventHandler(this.SelectDescDir_Click);
+            // 
+            // SrcRootDir
+            // 
+            resources.ApplyResources(this.SrcRootDir, "SrcRootDir");
+            this.SrcRootDir.Name = "SrcRootDir";
+            // 
+            // DescDir
+            // 
+            resources.ApplyResources(this.DescDir, "DescDir");
+            this.DescDir.Name = "DescDir";
+            // 
+            // SrcSubDir
+            // 
+            resources.ApplyResources(this.SrcSubDir, "SrcSubDir");
+            this.SrcSubDir.Name = "SrcSubDir";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // groupBox2
+            // 
+            resources.ApplyResources(this.groupBox2, "groupBox2");
+            this.groupBox2.Controls.Add(this.LogTextBox);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.TabStop = false;
+            // 
+            // LogTextBox
+            // 
+            resources.ApplyResources(this.LogTextBox, "LogTextBox");
+            this.LogTextBox.BackColor = System.Drawing.Color.White;
+            this.LogTextBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.LogTextBox.Name = "LogTextBox";
+            // 
+            // StartCopyBtn
+            // 
+            resources.ApplyResources(this.StartCopyBtn, "StartCopyBtn");
+            this.StartCopyBtn.Name = "StartCopyBtn";
+            this.StartCopyBtn.UseVisualStyleBackColor = true;
+            this.StartCopyBtn.Click += new System.EventHandler(this.StartCopyBtn_Click);
+            // 
+            // FBDBaseDir
+            // 
+            resources.ApplyResources(this.FBDBaseDir, "FBDBaseDir");
+            // 
+            // FBDDescDir
+            // 
+            resources.ApplyResources(this.FBDDescDir, "FBDDescDir");
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.选项ToolStripMenuItem,
+            this.帮助ToolStripMenuItem,
+            this.TSMIClose});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // 选项ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.选项ToolStripMenuItem, "选项ToolStripMenuItem");
+            this.选项ToolStripMenuItem.Name = "选项ToolStripMenuItem";
+            // 
+            // 帮助ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.帮助ToolStripMenuItem, "帮助ToolStripMenuItem");
+            this.帮助ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.关于本软件ToolStripMenuItem,
+            this.联系作者ToolStripMenuItem});
+            this.帮助ToolStripMenuItem.Name = "帮助ToolStripMenuItem";
+            // 
+            // 关于本软件ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.关于本软件ToolStripMenuItem, "关于本软件ToolStripMenuItem");
+            this.关于本软件ToolStripMenuItem.Name = "关于本软件ToolStripMenuItem";
+            // 
+            // 联系作者ToolStripMenuItem
+            // 
+            resources.ApplyResources(this.联系作者ToolStripMenuItem, "联系作者ToolStripMenuItem");
+            this.联系作者ToolStripMenuItem.Name = "联系作者ToolStripMenuItem";
+            // 
+            // TSMIClose
+            // 
+            resources.ApplyResources(this.TSMIClose, "TSMIClose");
+            this.TSMIClose.Name = "TSMIClose";
+            this.TSMIClose.Click += new System.EventHandler(this.TSMIClose_Click);
+            // 
+            // XCopyFast
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.StartCopyBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "XCopyFast";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button SelectDescDir;
+        private System.Windows.Forms.TextBox DescDir;
+        private System.Windows.Forms.TextBox SrcSubDir;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button StartCopyBtn;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox SrcRootDir;
+        private System.Windows.Forms.Button SelectSrcRootDir;
+        private System.Windows.Forms.FolderBrowserDialog FBDBaseDir;
+        private System.Windows.Forms.FolderBrowserDialog FBDDescDir;
+        private System.Windows.Forms.TextBox LogTextBox;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem 选项ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 帮助ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem TSMIClose;
+        private System.Windows.Forms.ToolStripMenuItem 关于本软件ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem 联系作者ToolStripMenuItem;
+    }
+}
+
---
+++ XCopyFast/XCopyFast.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace XCopyFast
+{
+    public partial class XCopyFast : Form
+    {
+        public XCopyFast()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 开始复制目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartCopyBtn_Click(object sender, EventArgs e)
+        {
+            String baseDir      = SrcRootDir.Text.Replace("\\", "/");
+            String [] subFiles  = (SrcSubDir.Text.Replace("\\", "/").Replace("，", ",")).Split('\r');
+            String descDir      = DescDir.Text.Replace("\\", "/");
+            try {
+                LogTextBox.Text = "";
+                foreach(String file in subFiles) {
+                    String srcFile  = file.Replace("\n", "");
+                    CopyFile(baseDir + "/" + srcFile, descDir + "/" + srcFile);
+                }
+            } catch(Exception ex) {
+                MessageBox.Show(ex.Message, "操作提示", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+
+        /// <summary>
+        /// 创建目录
+        /// </summary>
+        /// <param name="newDir"></param>
+        private void CreateDirectory(String newDir)
+        {
+            String [] dirs  = newDir.Split('/');
+            String dirPath  = "";
+            foreach(String dir in dirs) {
+                dirPath     += dir + "/";
+                DirectoryInfo dirInfo   = new DirectoryInfo(dirPath);
+                if(dirInfo.Exists) {
+                    continue;
+                }
+                dirInfo.Create();
+            }
+        }
+        
+        /// <summary>
+        /// 复制文件
+        /// </summary>
+        /// <param name="srcFile">源文件</param>
+        /// <param name="descFile">目标文件</param>
+        private void CopyFile(String srcFile, String descFile)
+        {
+            FileInfo file   = new FileInfo(srcFile);
+            if(!file.Exists) {
+                return;
+            }
+            CreateDirectory(_GetParentDir(descFile));
+            FileInfo descInfo   = new FileInfo(descFile);
+            if(descInfo.Exists) {
+                LogTextBox.Text     += "[Exists]\t" + srcFile + "\r\n";
+                return;
+            }
+            LogTextBox.Text     += "[Copying]\t" + srcFile + "\r\n";
+            File.Copy(file.FullName, descFile, true);
+            LogTextBox.Text     += "[Done]\t" + srcFile + "\r\n";
+        }
+
+        /// <summary>
+        /// 得到当前文件所在目录
+        /// </summary>
+        /// <param name="file"></param>
+        /// <returns></returns>
+        private String _GetParentDir(String file)
+        {
+            int loc     = file.LastIndexOf("/");
+
+            return file.Substring(0, loc);
+        }
+
+        /// <summary>
+        /// 复制目录
+        /// </summary>
+        /// <param name="srcDir"></param>
+        /// <param name="tgtDir"></param>
+        private void CopyDirectory(string srcDir, string tgtDir)
+        {
+            DirectoryInfo source = new DirectoryInfo(srcDir);
+            DirectoryInfo target = new DirectoryInfo(tgtDir);
+            if (target.FullName.StartsWith(source.FullName, StringComparison.CurrentCultureIgnoreCase)) {
+                throw new Exception("父目录不能拷贝到子目录！");
+            }
+            if (!source.Exists) {
+                return;
+            }
+            if (!target.Exists) {
+                target.Create();
+            }
+            FileInfo[] files = source.GetFiles();
+            for (int i = 0; i < files.Length; i++) {
+                File.Copy(files[i].FullName, target.FullName + @"\" + files[i].Name, true);
+                LogTextBox.Text   += "[Copying]" + files[i].FullName + "\r\n"; 
+            }
+            DirectoryInfo[] dirs = source.GetDirectories();
+            for (int j = 0; j < dirs.Length; j++) {
+                CopyDirectory(dirs[j].FullName, target.FullName + @"\" + dirs[j].Name);
+            }
+        }
+
+        /// <summary>
+        /// 选择源根目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectSrcRootDir_Click(object sender, EventArgs e)
+        {
+            DialogResult dr     = FBDBaseDir.ShowDialog();
+            if(dr != DialogResult.OK) {
+                return;
+            }
+            SrcRootDir.Text     = FBDBaseDir.SelectedPath;
+        }
+
+        /// <summary>
+        /// 选择目标目录
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void SelectDescDir_Click(object sender, EventArgs e)
+        {
+            DialogResult dr     = FBDDescDir.ShowDialog();
+            if(dr != DialogResult.OK) {
+                return;
+            }
+            DescDir.Text        = FBDDescDir.SelectedPath;
+        }
+
+        private void TSMIClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        } 
+    }
+}
